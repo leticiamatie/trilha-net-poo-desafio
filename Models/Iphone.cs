7e814d77cb9c92d603c4b6ad1e1229b66158f39d
Leticia Matie <matieleticia@gmail.com>
2023-12-07 18:58:44 -0300

    Add files via upload

---
+++ Models/Iphone.cs
@@ -0,0 +1,16 @@
+namespace DesafioPOO.Models
+{
+    // Herda da classe "Smartphone"
+    public class Iphone : Smartphone
+    {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        // Sobrescreve o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} no iPhone...");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia (numero: "911111111", modelo: "Modelo 1", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Nokia (numero: "922222222", modelo: "Modelo 2", imei: "222222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
